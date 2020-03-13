e03736df6a9642c83f7a89326357bca4d4fe40c1
f17014612 <theo.fievet@etu.univ-amu.fr>
2020-03-13 18:31:20 +0100

    nasm update

--- src/nasm/C3a2nasm.java
+++ src/nasm/C3a2nasm.java
@@ -1,9 +1,9 @@
 package nasm;
 
 import c3a.*;
+import sa.SaLDec;
 import ts.Ts;
 import ts.TsItemFct;
-import ts.TsItemVar;
 
 public class C3a2nasm implements C3aVisitor<NasmOperand> {
     private C3a c3a;
@@ -38,6 +38,22 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
 
     @Override
     public NasmOperand visit(C3aInstFBegin inst) {
+        NasmRegister reg_ebp = nasm.newRegister();
+        reg_ebp.colorRegister(Nasm.REG_EBP);
+
+        NasmRegister reg_esp = nasm.newRegister();
+        reg_esp.colorRegister(Nasm.REG_ESP);
+
+        nasm.ajouteInst(new NasmPush(null, reg_ebp, ""));
+        nasm.ajouteInst(new NasmMov(null, reg_ebp, reg_esp, ""));
+
+        currentFct = inst.val;
+        SaLDec dec = currentFct.saDecFonc.getVariable();
+        if (dec != null) {
+            NasmConstant constant = new NasmConstant(4*dec.length());
+            nasm.ajouteInst(new NasmSub(null, reg_esp, constant, ""));
+        }
+
         return null;
     }
 
@@ -96,6 +112,19 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
 
     @Override
     public NasmOperand visit(C3aInstFEnd inst) {
+        SaLDec dec = currentFct.saDecFonc.getVariable();
+        if (dec != null) {
+            NasmRegister reg_esp = nasm.newRegister();
+            reg_esp.colorRegister(Nasm.REG_ESP);
+            NasmConstant constant = new NasmConstant(4*dec.length());
+            nasm.ajouteInst(new NasmAdd(null, reg_esp, constant, ""));
+        }
+
+        NasmRegister reg_ebp = nasm.newRegister();
+        reg_ebp.colorRegister(Nasm.REG_EBP);
+
+        nasm.ajouteInst(new NasmPop(null, reg_ebp, ""));
+        nasm.ajouteInst(new NasmRet(null, ""));
         return null;
     }
 
@@ -126,6 +155,13 @@ public class C3a2nasm implements C3aVisitor<NasmOperand> {
 
     @Override
     public NasmOperand visit(C3aInstWrite inst) {
+        NasmRegister reg_eax = nasm.newRegister();
+        reg_eax.colorRegister(Nasm.REG_EAX);
+        NasmOperand operand = inst.op1.accept(this);
+
+        nasm.ajouteInst(new NasmMov(null, reg_eax, operand, ""));
+        nasm.ajouteInst(new NasmCall(null, new NasmLabel("printf"), ""));
+
         return null;
     }
 

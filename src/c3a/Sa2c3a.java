3db109a1736f0e72e98c81445ea3c7fb34229859
f17014612 <theo.fievet@etu.univ-amu.fr>
2020-03-13 16:47:10 +0100

    c3a update

--- src/c3a/Sa2c3a.java
+++ src/c3a/Sa2c3a.java
@@ -10,22 +10,25 @@ public class Sa2c3a extends SaDepthFirstVisitor<C3aOperand> {
     private Ts table;
     private Ts tableLocale;
 
-    public Sa2c3a(SaNode root) {
+    public Sa2c3a(SaNode root, Ts table) {
+        this.table = table;
         c3a = new C3a();
         root.accept(this);
     }
 
+    public C3a getC3a() {
+        return c3a;
+    }
+
     public C3aOperand visit(SaExpInt node){
         C3aConstant constant = new C3aConstant(node.getVal());
-        C3aInstAffect affect = new C3aInstAffect(constant, constant, "constante");
+        C3aInstAffect affect = new C3aInstAffect(constant, constant, "constante"); //?
         c3a.ajouteInst(affect);
         return null;
     }
 
     public C3aOperand visit(SaVarSimple node){
         TsItemVar var = table.getVar(node.getNom());
-
-        C3aVar varSimple = new C3aVar(var);
         return null;
     }
 
